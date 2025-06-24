35d5f17087d5aa9f8b9f6bb4fcadf40c442f2c09
Eric Branson <eric.branson@gmail.com>
2025-06-24 15:13:22 -0600

    name form schema

--- src/lib/formSchemas/schemas.ts
+++ src/lib/formSchemas/schemas.ts
@@ -84,3 +84,10 @@ export type DateSearchFormSchema = typeof dateSearchFormSchema;
 
 export const blankFormSchema = v.object({});
 export type BlankFormSchema = typeof blankFormSchema;
+
+export const nameFormSchema = v.object({
+   familyName: v.pipe(v.string(), v.minLength(1), v.maxLength(255)),
+   givenName: v.pipe(v.string(), v.minLength(1), v.maxLength(255)),
+   organizationName: v.nullable(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
+});
+export type NameFormSchema = typeof nameFormSchema;
