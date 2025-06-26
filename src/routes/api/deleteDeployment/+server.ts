fab46c932c373da09f76774fdd369dc9900a0698
Eric Branson <eric.branson@gmail.com>
2025-06-26 11:31:33 -0600

    init

---
+++ src/routes/api/deleteDeployment/+server.ts
@@ -0,0 +1,34 @@
+import { prisma } from '$lib/server/prisma';
+import { vercelClient } from '$lib/server/vercel';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async (event) => {
+   const body = await event.request.json();
+   const { userId } = body;
+   if(!userId){
+      return new Response(JSON.stringify('User Id not provided'), {status:400})
+   }
+   const dbDeployments = await prisma.vercelDeployment.findMany({
+      where: {
+         userId,
+      }
+   })
+   const projectList = await vercelClient.projects.getProjects({});
+   console.log(projectList)
+   for(const dbDeployment of dbDeployments){
+      for(const project of projectList.projects){
+         if(project.name === `mms-demo-${dbDeployment.id}`){
+            await vercelClient.projects.deleteProject({
+               idOrName:project.id
+            })
+            await prisma.vercelDeployment.delete({
+               where:{
+                  id: dbDeployment.id
+               }
+            })
+         }
+      }
+   }
+   console.log('deleted')
+   return new Response(JSON.stringify('deployments deleted'), {status: 200});
+};
