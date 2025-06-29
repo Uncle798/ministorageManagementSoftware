6a705f9756e09f3811b9e52e8db5a0e4e41058f9
Eric Branson <eric.branson@gmail.com>
2025-06-29 15:41:34 -0600

    uses api to send email. currently sends 2 emails for some reason

--- src/routes/register/emailVerification/+page.server.ts
+++ src/routes/register/emailVerification/+page.server.ts
@@ -13,107 +13,59 @@ export const load:PageServerLoad = (async (event) => {
         fail(401);
         return{};
     }
-    if(event.locals.user.emailVerified){
-        redirect(302, '/accountSettings')
-    }
     const emailVerificationForm = await superValidate(valibot(emailVerificationFormSchema));
-    const verification = await prisma.verification.findFirst({
-        where: {
-            userId: event.locals.user?.id
-        }
-    });
-    if(!verification || verification?.expiresAt.getTime() <= Date.now() ){
-        const code = await generateEmailVerificationRequest(event.locals.user.id, event.locals.user.email!);
-        sendVerificationEmail(code, event.locals.user.email!);
-    }
     const redirectTo = event.url.searchParams.get('redirectTo');
     const unitNum = event.url.searchParams.get('unitNum')
     return { emailVerificationForm, redirectTo, unitNum };
 })
 
 export const actions: Actions ={
-    verify: async(event) => {
-        if(!event.locals.user){
-            redirect(302, '/login')
-        }
-        if(event.locals.user.emailVerified){
-            redirect(302, '/');
-        }
-        const formData = await event.request.formData();
-        const emailVerificationForm = await superValidate(formData, valibot(emailVerificationFormSchema));
-        if(!emailVerificationForm.valid){
-            return superFormFail(400, emailVerificationForm);
-        }
-        const { success, reset } = await ratelimit.emailVerification.limit(event.locals.user?.id || event.getClientAddress());
-        if(!success) {
-            const timeRemaining = Math.floor((reset - Date.now()) / 1000);
-            return message(emailVerificationForm, `Please wait ${timeRemaining} seconds before trying again`);
-        }
-        const verification = await prisma.verification.findFirst({
-            where: {
-                userId: event.locals.user.id
-            }
-        })
-        if(!verification){
-            return fail(401, {verify:'Not authenticated'})
-        }
-        if(verification?.expiresAt.getTime() <= Date.now() ){
-            const code = await generateEmailVerificationRequest(event.locals.user.id, event.locals.user.email!);
-            sendVerificationEmail(code, event.locals.user.email!)
-            return message(emailVerificationForm, 'The code had expired, we\'ve sent a new code to the email address on file');
-        }
-        if(verification.code !== emailVerificationForm.data.code){
-            message(emailVerificationForm, 'Code was invalid');
-        }
-        await prisma.verification.delete({
-            where: {
-                id: verification.id
-            }
-        })
-        await prisma.user.update({
-            where: {
-                id: event.locals.user.id
-            },
-            data: {
-                emailVerified: true
-            }
-        })
-        const redirectTo = event.url.searchParams.get('redirectTo');
-        const unitNum = event.url.searchParams.get('unitNum');
-        if(redirectTo === 'home'){
-            redirect(302, '/')
-        }
-        if(redirectTo){
-            redirect(302, `/${redirectTo}?unitNum=${unitNum}`)
-        }
-        return message(emailVerificationForm, 'Email verified');
-    },
-    resend: async(event) =>{
-        if(!event.locals.user){
-            return fail(401, {
-                resend: {
-                    message: 'Not authenticated'
-                }
-            })
-        }
-        if(event.locals.user.emailVerified){
-            redirect(302, '/users/' + event.locals.user.id)
-        }
-        const { success, reset } = await ratelimit.emailVerification.limit(event.locals.user.id);
-        if(!success){
-            const timeRemaining = Math.floor((reset - Date.now()) / 1000);
-            return fail(429, {
-                resend:{
-                    message: `Please wait ${timeRemaining} seconds before trying again`
-                }
-            })
-        }
-        if(!event.locals.user.email){
-            redirect(301, '/register')
-        }
-        const code = await generateEmailVerificationRequest(event.locals.user.id, event.locals.user.email);
-        sendVerificationEmail(code, event.locals.user.email);
-        const message = { message: 'Email sent'}
-        return { message }
-    }
+   verify: async(event) => {
+      if(!event.locals.user){
+         redirect(302, '/login?toastReason=unauthorized')
+      }
+      if(event.locals.user.emailVerified){
+         redirect(302, '/');
+      }
+      const formData = await event.request.formData();
+      const emailVerificationForm = await superValidate(formData, valibot(emailVerificationFormSchema));
+      if(!emailVerificationForm.valid){
+         return superFormFail(400, emailVerificationForm);
+      }
+      const { success, reset } = await ratelimit.emailVerification.limit(event.locals.user?.id || event.getClientAddress());
+      if(!success) {
+         const timeRemaining = Math.floor((reset - Date.now()) / 1000);
+         return message(emailVerificationForm, `Please wait ${timeRemaining} seconds before trying again`);
+      }
+      const verification = await prisma.verification.findFirst({
+         where: {
+               userId: event.locals.user.id
+         }
+      })
+      if(!verification){
+         return fail(401, {verify:'Not authenticated'})
+      }
+      if(verification?.expiresAt.getTime() <= Date.now() ){
+         const code = await generateEmailVerificationRequest(event.locals.user.id, event.locals.user.email!);
+         sendVerificationEmail(code, event.locals.user.email!)
+         return message(emailVerificationForm, 'The code had expired, we\'ve sent a new code to the email address on file');
+      }
+      if(verification.code !== emailVerificationForm.data.code){
+         message(emailVerificationForm, 'Code was invalid');
+      }
+      await prisma.verification.delete({
+         where: {
+               id: verification.id
+         }
+      })
+      await prisma.user.update({
+         where: {
+               id: event.locals.user.id
+         },
+         data: {
+               emailVerified: true
+         }
+      })
+      redirect(302, `/demo`)   
+   },
 }
