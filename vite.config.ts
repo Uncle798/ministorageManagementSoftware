0329b84c668f16d1557bffd639c31f5652551544
Eric Branson <eric.branson@gmail.com>
2025-07-03 11:41:18 -0600

    adapter auto

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [
+		tailwindcss(), 
+		sveltekit()
+	],
 	server: {
 		allowedHosts: [
 			'relative-oryx-endlessly.ngrok-free.app'
