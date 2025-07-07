bde6e6db5fc6649cc10dc3f344daa62b3d342144
Eric Branson <eric.branson@gmail.com>
2025-07-07 16:53:42 -0600

    init

---
+++ src/routes/api/destroyDemo/+server.ts
@@ -0,0 +1,57 @@
+import { NEON_PROJECT_ID } from '$env/static/private';
+import { neonClient } from '$lib/server/neon';
+import { prisma } from '$lib/server/prisma';
+import { vercelClient } from '$lib/server/vercel';
+import type { RequestHandler } from './$types';
+
+export const DELETE: RequestHandler = async (event) => {
+   const body = await event.request.json();
+   const { userId } = body;
+   if(!userId){
+      return new Response(JSON.stringify('User Id not specified'), {status:400});
+   }
+   try {
+      const user = await prisma.user.findUnique({
+         where: {
+            id: userId,
+         }
+      })
+      const dbDeployments = await prisma.vercelDeployment.findMany({
+         where: {
+            userId,
+         }
+      })
+      if(dbDeployments){
+         for(const deployment of dbDeployments){
+            await vercelClient.projects.deleteProject({
+               idOrName: deployment.vercelId ? deployment.vercelId : `demo-${user?.familyName}-${user?.givenName}`
+            })
+         }
+         await prisma.vercelDeployment.deleteMany({
+            where: {
+               userId
+            }
+         })
+      }
+      const dbBranches = await prisma.databaseBranch.findMany({
+         where: {
+            userId
+         }
+      })
+      if(dbBranches){
+         for(const branch of dbBranches){
+            if(branch.neonId){
+               await neonClient.deleteProjectBranch(NEON_PROJECT_ID, branch.neonId);
+            }
+         }
+         await prisma.databaseBranch.deleteMany({
+            where: {
+               userId,
+            }
+         })
+      }
+   } catch (error) {
+      console.error(error)
+   }
+   return new Response();
+};
