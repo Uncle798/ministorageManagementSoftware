b1067fded0de551497e7bbde0c26320e32801b24
Eric Branson <eric.branson@gmail.com>
2025-07-10 10:15:51 -0600

    adding OG image

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+   import { PUBLIC_URL } from "$env/static/public";
    interface Props {
       title: string
    }
@@ -7,4 +8,5 @@
 </script>
 <svelte:head>
 	<title>MMS | {title}</title>
+   <meta content={`${PUBLIC_URL}/api/og?message=${title}`} property="og:image"/>
 </svelte:head>
