34421be4083e5838d2ffc98f7d2458960e56ed0a
Eric Branson <eric.branson@gmail.com>
2025-07-13 11:52:50 -0600

    goto home after everything deleted

--- src/routes/destroyDemo/+page.svelte
+++ src/routes/destroyDemo/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { source } from 'sveltekit-sse';
    import type { PageData } from './$types';
+	import { fromStore } from 'svelte/store';
+	import { goto } from '$app/navigation';
 
    let { data }: { data: PageData } = $props();
    const message = source('/api/destroyDemo', {
@@ -8,6 +10,14 @@
          body: JSON.stringify({userId:data.user?.id})
       }
    }).select('message');
+   let messageState = $state(fromStore(message))
+   $effect(()=>{
+      if(messageState.current === 'Everything Deleted'){
+         setTimeout(()=>{
+            goto('/')
+         }, 2500)
+      }
+   })
 </script>
 
 <div class="mt-14 sm:mt-10 mx-2">
