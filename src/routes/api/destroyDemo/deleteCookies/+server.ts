20c9161de85152fe68824e84421597718d4b3ec1
Eric Branson <eric.branson@gmail.com>
2025-07-15 11:50:12 -0600

    not needed

--- src/routes/api/destroyDemo/deleteCookies/+server.ts
+++
@@ -1,9 +0,0 @@
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async (event) => {
-   const cookie = event.cookies.get('demoSession')
-   if(cookie){
-      event.cookies.delete('demoSession', {path: '/'})
-   }
-   return new Response(null, {status: 200});
-};
